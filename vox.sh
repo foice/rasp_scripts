2848f3c48cd18989cd0c8ca94b60caa806458ab7
foice <foice.news@gmail.com>
2017-08-14 23:18:46 +0200

    done some changes to silence levels. still useless!

--- vox.sh
+++ vox.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+threshold=$1
+
+if [[ "$1" == "" ]]; then
+threshold="1"
+fi
 
 while [ 1 ]
 do 
@@ -7,7 +12,10 @@ tempfile=temp.mp3
 rm -rf $tempfile
 
 #Listen for audio and record
-sox -d $tempfile silence 1 0.1 1% 1 0:0:5 1%
+#sox -d $tempfile silence 1 0.1 0.1% 1 0:0:5 0.1%
+sox -d $tempfile silence 1 5 "$threshold"% 1 0:00:02 "$threshold"%
+# syntax from http://www.linuxquestions.org/questions/linux-software-2/vox-recorder-and-audio-timer-recorder-598535/
+# rec recording.wav silence 1 5 2% 1 0:00:02 2%
 
 #Check if temp.mp3 is greater than 800 bytes so we don't get blank recordings added to the
 #database, if the file is below 800 bytes remove the file and restart.
