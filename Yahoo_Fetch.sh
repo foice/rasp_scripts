86d3edf5f08e97431527d47a52370e41a03ecce6
foice <foice.news@gmail.com>
2017-08-20 15:28:20 +0200

    made functions to get and write the quote added functionality to pass by TOR using torsocks if is able to locate torsocks in the PATH

--- Yahoo_Fetch.sh
+++ Yahoo_Fetch.sh
@@ -8,19 +8,40 @@ stock[0]="EURCHF=X"
 stock[1]="EURUSD=X"
 stock[2]="USDCHF=X"
 
-archive="archive"
 
+archive="archive" # folder in which to put the prvious days logs
+output_file_stub=`pwd -L`"/history_" # beginning part of the filename to save the daily log; beware it has its full path at the beginning! 
+
+########################################################################
+######################### function declarations ########################
+########################################################################
 
 debug_print(){
-   if [ $debug ]; then echo $1; fi 
+   if [ "$debug" = true ]; then 
+   	if [[ ! "$2" == '' ]]; then
+   		if [ $debug ]; then echo $1 >> $2 ; fi 
+   	else
+		echo $1;  
+	fi
+   fi
 }
 
-get_key(){
+get_key(){  # get a key for the timedb server API
   echo `cat key.conf`
 }
 
-# this function is to shuffle the elements of an array in bash
-shuffle() {
+
+have_torsocks(){ # check if the system has torsocks installed
+ torpath=`which torsocks`
+ if [ $torpath != '' ]; then
+  echo 1
+ else 
+  echo 0
+ fi
+}
+
+
+shuffle() { # this function is to shuffle the elements of an array in bash
    local i tmp size max rand
    size=${#stock[*]}
    max=$(( 32768 / size * size ))
@@ -31,7 +52,7 @@ shuffle() {
    done
 }
 
-check_oneday_old() {
+check_oneday_old() { # check if the first argument is a file more than 1 day old
   if [ -f "$1" ]; then
     if [[ $(find "$1" -mtime +1 -print) ]]; then
     #if [[ $(find "$1" -mtime +1 -print) ]]; then
@@ -61,15 +82,26 @@ check_dst_ny() {
   echo `less ny.xml | grep -o \<dst\>.*\</dst\> | grep -o \>.*\< | cut -c 2`
 }
 
-get_entry() {
- proxylen=$(cat "$1" | wc -l)	
- proxyid=$[ ( $RANDOM % $proxylen ) + 1 ]
- proxy=`awk -v r=$proxyid ' NR==r {print} ' "$1"`
- #if it is a proxy try to ping it before considering it valid
- echo $proxy
+get_entry() { # get a random entry from $1 and if it contains : then check it be an open port on that host
+ active=0
+ while [ $active -eq 0 ]; do
+	proxylen=$(cat "$1" | wc -l)	
+	proxyid=$[ ( $RANDOM % $proxylen ) + 1 ]
+  	proxy=`awk -v r=$proxyid ' NR==r {print} ' "$1"`
+  	#if it is a proxy try to ping it before considering it valid
+  	ip=$(echo $proxy | cut -f1 -d":")
+  	port=$(echo $proxy | cut -f2 -d":")
+	debug_print $ip":"$port nmap.log; 
+  	if [[ "$port" != "$proxy" ]]; then  # a second field existed and cut retuned it in $port
+   		active=`nmap -p $port $ip  | tail -1 | cut -f2 -d "(" | cut -f1 -d" "`
+  	else 
+   		active=1 
+  	fi
+  done 	
+  echo $proxy
 }
 
-check_quote(){
+check_quote(){ # check that is a valid float number
 	words=`echo "$1" | wc -w`
 	#echo $1 has $words words >> log 
 	re='^[0-9]+([.][0-9]+)?$'
@@ -80,9 +112,43 @@ check_quote(){
 	fi
 }
 
-# log to disk
-output_file_stub=`pwd -L`"/history_"
 
+get_quote(){	# get symbol $1 from from Yahoo Finance
+	s='https://download.finance.yahoo.com/d/quotes.csv?s=stock_symbol&f=l1'
+	symbol="$1"
+	_url=$( echo ${s/stock_symbol/$symbol} ) 
+	debug_print "Attempting  $_url" quotelog
+	
+	_quote=''
+	while [[ $(check_quote "$_quote") == 0 ]]; do
+			
+		if [ $havetorsock ]; then
+			debug_print "routed on tor at $(date -u)" routelog
+			_quote=$(torsocks curl -m 15 -s "$_url" ); #debug_print "quote: $quote ."
+		else
+			_proxy=$(get_entry proxies.list); #debug_print $proxy
+			_useragent=$(get_entry user_agent.list); #debug_print "$useragent"
+			_quote=$(curl -m 15 -x "$_proxy" -A "$_useragent" -s "$_url" ); #debug_print "quote: $quote ."
+			debug_print "routed on proxy $_proxy at $(date -u)" routelog
+		fi
+		done
+	echo "$_quote"
+}
+
+write_quote(){  # write value $1 to file $2 		
+	_quote="$1"
+	_output_file="$2"
+	echo -n $(date) >> "$_output_file";  #no new line -n
+	echo -n "  " >> "$_output_file";
+	echo -n ", " >> "$_output_file"; echo "$_quote" >> "$_output_file"; 
+}
+
+########################################################################
+#################### 	end of function declarations ###################
+########################################################################
+
+
+havetorsock=`have_torsocks`
 if [ ! -f key.conf ]; then
   echo "file key.conf is missing"
   exit
@@ -112,8 +178,6 @@ while [[ 1 -gt 0 ]]; do
       if  ! ([ $weekday == "5" ] && [ $time -gt $(($nycloses+1)) ]) ; then #is not friday after New York closes at 10:00 PM GMT (April to October). Adding one minute to allow write of archive on Friday.
         shuffle
 
-        # we get stock quotes from Yahoo
-        s='http://download.finance.yahoo.com/d/quotes.csv?s=stock_symbol&f=l1'
 
         n=${#stock[@]}
         n=$((n-1))
@@ -121,21 +185,12 @@ while [[ 1 -gt 0 ]]; do
         do
               output_file=$output_file_stub"${stock[$c]}"
 	      ### GET QUOTE
-	      quote=''
-	      while [[ $(check_quote "$quote") == 0 ]]; do	
-	      	proxy=$(get_entry proxies.list); #debug_print $proxy
-	     	useragent=$(get_entry user_agent.list); #debug_print "$useragent"
-              	quote=$(curl -m 15 -x $proxy -A "$useragent" -s $(echo ${s/stock_symbol/${stock[$c]}}) ); #debug_print "quote: $quote ."
-	      done
-	      debug_print "final quote: $quote `date -u`"
+	      quote=$( get_quote ${stock[$c]} );  debug_print "got a quote: $quote `date -u`"
               ### WRITE QUOTE
-	      echo -n $(date) >> $output_file;  #no new line -n
-              echo -n "  " >> $output_file;
-              echo -n ", " >> $output_file; echo $quote >> $output_file; 
+	      write_quote "$quote" "$output_file"; debug_print "$quote -> $output_file"
               ### UPLOAD TO DB
 	      if [ $RANDOM -gt $((32767*95/100)) ]; then
-                action="YahooData_Dropbox_Upload.sh upload $output_file ${output_file##*/}"
-                #echo $action
+                action="YahooData_Dropbox_Upload.sh upload $output_file ${output_file##*/}"; debug_print "$action"
                 $action >> dblog
               fi
 
