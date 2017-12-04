d92aa4cf60fa809904e78d6fe19127d840dacfb9
Roberto <foice.news@gmail.com>
2017-12-04 15:01:01 +0100

    print

--- Yahoo_Fetch.sh
+++ Yahoo_Fetch.sh
@@ -13,9 +13,9 @@ readarray stock < ./stocks.conf
 
 echo "${stock[*]} will be retrieved and stored"
 
-echo stock[0]
-echo stock[1]
-echo stock[2]
+echo $stock[0]
+echo $stock[1]
+echo $stock[2]
 
 
 archive="archive" # folder in which to put the prvious days logs
