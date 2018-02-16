3c19ac3974f40b2660a1bd663e22e6fc9e8391ed
foice <foice.news@gmail.com>
2018-02-16 23:05:18 +0100

    added file name and resolution

--- scan_image.py
+++ scan_image.py
@@ -1,33 +1,44 @@
-
+#!/usr/bin/python 
 import pyinsane2
+import argparse
 
-pyinsane2.init()
-try:
-	devices = pyinsane2.get_devices()
-	assert(len(devices) > 0)
-	device = devices[0]
-	print("I'm going to use the following scanner: %s" % (str(device)))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output_name", default="scansione.jpg", help="string to search into")
+    parser.add_argument("-r", "--resolution", default="150", help="string to search into")
+    args = parser.parse_args()
+    output_name=args.output_name
+    resolution=args.resolution 
 
-	pyinsane2.set_scanner_opt(device, 'resolution', [300])
+    pyinsane2.init()
 
-# Beware: Some scanners have "Lineart" or "Gray" as default mode
-# better set the mode everytime
-#	pyinsane2.set_scanner_opt(device, 'mode', 'Gray')
+    try:
+            devices = pyinsane2.get_devices()
+            assert(len(devices) > 0)
+            device = devices[0]
+            print("I'm going to use the following scanner: %s" % (str(device)))
 
-# Beware: by default, some scanners only scan part of the area
-# they could scan.
-	pyinsane2.maximize_scan_area(device)
+            pyinsane2.set_scanner_opt(device, 'resolution', [int(resolution)])
 
-	scan_session = device.scan(multiple=False)
-	try:
-		while True:
-			scan_session.scan.read()
-	except EOFError:
-		pass
-	image = scan_session.images[-1]
-	image.save('output_file', "JPEG")
-	print("Done")
-finally:
-	pyinsane2.exit()
+    # Beware: Some scanners have "Lineart" or "Gray" as default mode
+    # better set the mode everytime
+    #	pyinsane2.set_scanner_opt(device, 'mode', 'Gray')
 
+    # Beware: by default, some scanners only scan part of the area
+    # they could scan.
+            pyinsane2.maximize_scan_area(device)
 
+            scan_session = device.scan(multiple=False)
+            try:
+                    while True:
+                            scan_session.scan.read()
+            except EOFError:
+                    pass
+            image = scan_session.images[-1]
+            image.save(output_name, "JPEG")
+            print("Done")
+    finally:
+            pyinsane2.exit()
+
+if __name__ == '__main__':
+    main()
--- scan_print.sh
+++ scan_print.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source /home/pi/b
 echo "Acquisisco l'immagnie"
-/home/pi/rasp_scripts/scan_image.py
+/home/pi/rasp_scripts/scan_image.py -o temp.jpg 
 echo "Stampo la copia"
-lpr -P HP_LaserJet_1000 output_file 
+lpr -P HP_LaserJet_1000 temp.jpg 
 echo "Attendere 1 minuto"
 sleep 60
