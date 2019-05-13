8c67f8b86a2120641d1419a557886b7df027257e
foice <foice.news@gmail.com>
2019-05-13 12:57:11 +0200

    boh

--- urlwatchmail.sh
+++ urlwatchmail.sh
@@ -6,8 +6,18 @@ header='To:Roberto Franceschini <roberto.franceschini@uniroma3.it>\nFrom:Roberto
 
 if [ -z "$body" ]
 then
-      echo " is empty"
+	echo " no changes in any webpage"
+        # refresh the passphrase in the cache
+	cd /run/user/$(id -u)
+	# make the tmpfs folder 
+	gpgtmp=gpgtmp
+	mkdir $gpgtmp
+	chmod 0700 $gpgtmp
+	cd $gpgtmp
+	/usr/bin/gpg --quiet --for-your-eyes-only --no-tty --decrypt /home/pi/.mail/.msmtp-credentials.gpg | /usr/bin/gpg --encrypt -r 3149BB87487375139BBF8FBB5A0CB586D3DA04 > ll.gpg
+	/usr/bin/srm ll.gpg
+	cd
 else
-      echo " is NOT empty"
-      echo -e "$header" "\n\n" "$body" | /usr/bin/msmtp --file=/home/pi/.msmtprc -a fisica -- roberto.franceschini@uniroma3.it
+	echo "Changes found in some webapge!"
+	echo -e "$header" "\n\n" "$body" | /usr/bin/msmtp --file=/home/pi/.msmtprc -a fisica -- $(cat /home/pi/rasp_scripts/urlwatch.recipient )
 fi
