406a96edd1ada1237400c0a3a0ee34523802c72f
foice <foice.news@gmail.com>
2019-10-13 12:59:25 +0200

    automatic output from Notebook

--- TemperatureMonitor/Temperature Monitor.py
+++ TemperatureMonitor/Temperature Monitor.py
@@ -1,74 +1,66 @@
 
 # coding: utf-8
 
-# In[1]:
+# # Make HTML and PNG
+
+# In[14]:
 
 
 import pandas as pd
-from bokeh.io import show, output_file, save
-from bokeh.layouts import column
-from bokeh.models import ColumnDataSource, RangeTool
-from bokeh.plotting import figure
 import time;  # This is required to include time module.
 import datetime
+import optparse
 
 
 # In[2]:
 
 
-df=pd.read_csv('temp.csv')
+import utils
 
 
-# In[3]:
+# In[12]:
 
 
-df.columns=['Epoch','Temperature','Humidity']
-
-
-# In[4]:
-
-
-df['Date']=(pd.to_datetime(df['Epoch'],unit='s')) 
-
-
-# In[5]:
-
-
-dt_now = datetime.datetime.now()
-yesterday = dt_now - datetime.timedelta(days=1)
+VERSION=1.0
+usage='TemperatureMonitor.py [-f temp.csv]'
+epilog=''
 
 
 # In[6]:
 
 
-data = df[['Date','Temperature']][df['Date']>yesterday]
+def SelectDataAndPlot(df,cols=None,dateField='Date',days=7,output_format='png'):
+    """ Select a range of days starting `days` ago from its first positional argument"""
+    if cols is not None:
+        dt_now = datetime.datetime.now()
+        beginning = dt_now - datetime.timedelta(days=days)
+        data = df[cols][df[dateField]>beginning]
+        utils.makeDateSeriesPlot(data,output_format=output_format,file_name_root=str(days))
+    else:
+        print('cols cannot be None')
 
 
-# In[14]:
+# In[ ]:
 
 
-p = figure(plot_height=300, plot_width=800, tools="xpan,reset,save,hover", toolbar_location='left',
-           x_axis_type="datetime", x_axis_location="above",
-           background_fill_color="#efefef", x_range=(data['Date'].min(), data['Date'].max()))
+def main():    
+    parser = optparse.OptionParser(usage=usage, version=VERSION,epilog=epilog)
+    parser.add_option('-f', '--file',action='store',default='temp.csv',help="CSV file to process")
+    parser.add_option('-d', '--days',action='store',default=365,type=int,help="number of days to plot")
+    options, args = parser.parse_args()
+    days=options.days
+    fileCSV=options.file
+    ##########################################
+    df=pd.read_csv(fileCSV)
+    df.columns=['Epoch','Temperature','Humidity']
+    df['Date']=(pd.to_datetime(df['Epoch'],unit='s'))
+    ##########################################
+    SelectDataAndPlot(df,cols=['Date','Temperature'],days=days)
 
-p.line('Date', 'Temperature', source=data)
-p.yaxis.axis_label = 'Temperature [C]'
 
-select = figure(title="Drag the middle and edges of the selection box to change the range above",
-                plot_height=130, plot_width=800, y_range=p.y_range,
-                x_axis_type="datetime", y_axis_type=None,
-                tools="", toolbar_location=None, background_fill_color="#efefef")
+# In[ ]:
 
-range_tool = RangeTool(x_range=p.x_range)
-range_tool.overlay.fill_color = "navy"
-range_tool.overlay.fill_alpha = 0.2
 
-select.line('Date', 'Temperature', source=data)
-select.ygrid.grid_line_color = None
-select.add_tools(range_tool)
-select.toolbar.active_multi = range_tool
-
-#show(column(p, select))
-output_file("temp.html")
-save(column(p, select))
+if __name__ == "__main__":
+    main()
 
