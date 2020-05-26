88dccf1aa4bc2b5029a02ef364e3a8c7a7a5b300
Foice <foice.news@gmail.com>
2020-05-26 09:09:06 -0400

    added log

--- check_ipr.sh
+++ check_ipr.sh
@@ -1,5 +1,6 @@
 test=`ip r | wc -l`
 if [ $test == 4 ]; then
 	echo "ip r has 4 lines"
-	/home/pi/rasp_scripts/wifi-to-eth-route.sh 
+	/home/pi/rasp_scripts/wifi-to-eth-route.sh
+        echo "corrected routings at $(date)" >> ~/wifi-to-eth-route.log	
 fi
