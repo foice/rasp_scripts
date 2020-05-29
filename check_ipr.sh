0893cb0100bf404efa14c52b8d118cd39aeea837
Foice <foice.news@gmail.com>
2020-05-29 15:54:40 +0200

    proper check of the presence of a default route for eth0

--- check_ipr.sh
+++ check_ipr.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 test=`ip r | wc -l`
-if [ $test == 4 ]; then
-	echo "ip r has 4 lines"
+test=`ip r | grep  default | grep eth0 | wc -l`
+if [ $test -gt 0 ]; then
+	echo "ip r has a default route foe eth0. Needs to be removed"
 	/home/pi/rasp_scripts/wifi-to-eth-route.sh
         echo "corrected routings at $(date)" >> ~/wifi-to-eth-route.log	
 fi
