607a17eada9df2f789e477738292112692c4ae1a
Foice <foice.news@gmail.com>
2020-06-10 14:38:38 +0200

    dropbox path

--- fetch_network_usage.sh
+++ fetch_network_usage.sh
@@ -2,5 +2,5 @@
 today=`date +%Y_%d_%m`
 cd /home/pi/ipfm_logs/
 dropbox_uploader.sh upload $today ./ipfm_logs/
-dropbox_uploader.sh upload subnet/"$today" ./ipfm_logs/
+dropbox_uploader.sh upload subnet/"$today" ./ipfm_logs/subnet/
 cd
