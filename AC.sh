efb9ba1636aa6d7d4b645e4ba1185f561a51dcbc
foice <foice.news@gmail.com>
2020-06-17 15:27:50 +0200

    boh

--- AC.sh
+++ AC.sh
@@ -6,16 +6,33 @@ then
 	temp=$1
 fi
 
-echo "Will keep T=$temp C"
+echo "Will keep T=$temp °C"
 
 while [ 1 -ge 0 ]
 do
-	if [ $(wget -qO- 192.168.114.200/last_temp.csv | cut -f2 -d"," | cut -f1 -d".") -ge $temp  ]
+ 
+# -q quiet
+# -c nb of pings to perform
+ping -q -c5 192.168.114.200  > /dev/null
+ 
+	if [ $? -eq 0 ]
+	then
+		echo "Host is reachable"
+		current_temperature=$(wget -qO- 192.168.114.200/last_temp.csv | cut -f2 -d"," | cut -f1 -d".")
+	else
+		curl wttr.in | head -4 | tail -1 > t		
+		current_temperature=$(more t | sed 's/\x1B\[[0-9;]\+[A-Za-z]//g'  | cut -f3 -d".")
+		echo "Going from weather"
+	fi
+	
+	echo "The current temperature is $current_temperature °C"
+
+	if [ $current_temperature -ge $temp  ]
 	then
 		echo "Need to switch ON" # if temperature >  threshold then switch on
 		irsend SEND_ONCE achaier on
 	fi
-	if [ $(wget -qO- 192.168.114.200/last_temp.csv | cut -f2 -d"," | cut -f1 -d".") -lt $temp  ]
+	if [ $current_temperature -lt $temp  ]
 	then
 		echo "Need to switch OFF" # if temperature <  threshold then switch off
 		irsend SEND_ONCE achaier off 
