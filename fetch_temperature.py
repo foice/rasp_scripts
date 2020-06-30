9be2bc36ae2490476db88eee2dc281ba93465ea5
foice <foice.news@gmail.com>
2020-06-30 09:26:53 +0200

    works when not gateway if ESP broacasts ESSID

--- fetch_temperature.py
+++ fetch_temperature.py
@@ -1,46 +1,79 @@
 #!/usr/bin/env python3
 import urllib.request
+from bs4 import BeautifulSoup
 import pandas as pd 
 import urllib
+import re
 import os
 from socket import timeout
 import logging 
 from urllib.error import HTTPError, URLError
 import socket
 from termcolor import colored
+from pathlib import Path
 
-os.system("/home/pi/rasp_scripts/arp2hostname.sh > .arp ")
-arp=pd.read_csv('.arp',sep='\s+',names=["ip","mac","hostname"])
-hosts=pd.read_csv('~/known_macs',sep='\s+',names=["1","2","3","mac","long_hostname"])
-# print(arp)
-arp=arp[arp["hostname"]==arp["hostname"]  ]
-print(arp)
 
-for hostName in arp["hostname"].unique(): 
-	if 'ESP-' in hostName:
-		print(colored( 'downloading data for '+hostName,'green'))
-		IP=arp[ arp["hostname"]==hostName ]['ip'].unique()
-		IP=IP[0]
-		mac=arp[ arp["hostname"]==hostName ]['mac'].unique()
-		mac=mac[0]
-		long_hostname=hosts[ hosts["mac"]==mac ]['long_hostname'].unique()[0]
-		print('IP: '+IP," (",mac,":"+long_hostname+")")
-		url="http://"+IP+"/temp.csv"
-		file_name=long_hostname+"_"+IP+".csv"
-		print('URL:',url)
-		print('file:',file_name)
-		try:
-			with urllib.request.urlopen(url,timeout=100) as response, open(file_name, 'wb') as out_file:
-				data = response.read() # a `bytes` object
-				out_file.write(data)
-		except ConnectionResetError:
-			logging.error('ConnectionResetError:')
-		except HTTPError as error:
-			logging.error('Data not retrieved because %s\nURL: %s', error, url)
-		except URLError as error:
-			if isinstance(error.reason, socket.timeout):
-				logging.error('socket timed out - URL %s', url)
-			else:
-				logging.error('some other error happened')
+
+thisIsGateway=False
+my_file = Path("/var/lib/dnsmasq/dnsmasq.leases")
+if my_file.is_file():
+	# file exists
+	thisIsGateway=True
+
+def url2filename(url,file_name):
+	try:
+		with urllib.request.urlopen(url,timeout=100) as response, open(file_name, 'wb') as out_file:
+			data = response.read() # a `bytes` object
+			out_file.write(data)
+	except ConnectionResetError:
+		logging.error('ConnectionResetError:')
+	except HTTPError as error:
+		logging.error('Data not retrieved because %s\nURL: %s', error, url)
+	except URLError as error:
+		if isinstance(error.reason, socket.timeout):
+			logging.error('socket timed out - URL %s', url)
 		else:
-			logging.info('Access successful.')
+			logging.error('some other error happened')
+	else:
+		logging.info('Access successful.')
+
+if thisIsGateway:
+	os.system("/home/pi/rasp_scripts/arp2hostname.sh > .arp ")
+	arp=pd.read_csv('.arp',sep='\s+',names=["ip","mac","hostname"])
+	hosts=pd.read_csv('~/known_macs',sep='\s+',names=["1","2","3","mac","long_hostname"])
+	# print(arp)
+	arp=arp[arp["hostname"]==arp["hostname"]  ]
+	print(arp)
+
+	for hostName in arp["hostname"].unique(): 
+		if 'ESP-' in hostName:
+			print(colored( 'downloading data for '+hostName,'green'))
+			IP=arp[ arp["hostname"]==hostName ]['ip'].unique()
+			IP=IP[0]
+			mac=arp[ arp["hostname"]==hostName ]['mac'].unique()
+			mac=mac[0]
+			long_hostname=hosts[ hosts["mac"]==mac ]['long_hostname'].unique()[0]
+			print('IP: '+IP," (",mac,":"+long_hostname+")")
+			url="http://"+IP+"/temp.csv"
+			file_name=long_hostname+"_"+IP+".csv"
+			print('URL:',url)
+			print('file:',file_name)
+			url2filename(url,file_name)
+
+else:
+	os.system(' sudo iwlist wlan0 scan | grep ESSID | grep @ | cut -f2 -d"@"   > .ips ')
+	ips=pd.read_csv('.ips',sep='"',names=["ip"],index_col=False )
+	print(ips)
+	for ip in ips['ip'].unique():
+		url="http://"+ip
+		print('working on ',ip)	
+		html = urllib.request.urlopen(url)
+		bsh = BeautifulSoup(html.read(), 'html.parser')
+		h1=str(bsh.h1)
+		name=h1.split('ESP')[0] #.split('<h1>')[0]
+		#print(name)
+		name=name.split('>')[1]
+		name = re.sub(r"\s+", '_', name)
+		print(name)
+		file_name=name+'_'+str(ip)+'.csv'
+		url2filename(url+'/temp.csv',file_name)
