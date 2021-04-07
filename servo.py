4eb5fe433d426828075f48fff633a920cbacfe57
foice <foice.news@gmail.com>
2021-04-07 18:47:27 +0200

    added arguments for position x,y   span,center,steps

--- servo.py
+++ servo.py
@@ -4,6 +4,22 @@ import RPi.GPIO as GPIO
 import time
 from picamera import PiCamera
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-x", "--xpos", default=0, type=int, help="x position")
+parser.add_argument("-y", "--ypos", default=0, type=int, help="y position")
+parser.add_argument("-c", "--center", default=90, type=int, help="center position")
+parser.add_argument("-s", "--span", default=20, type=int, help="span of the scan")
+parser.add_argument("-n", "--steps", default=3, type=int, help="span of the scan")
+args = parser.parse_args()
+
+cli_x=args.xpos
+cli_y=args.ypos
+
+center=args.center
+span=args.span
+steps=args.steps
 
 #Set function to calculate percent from angle
 def angle_to_percent (angle) :
@@ -25,14 +41,23 @@ GPIO.setwarnings(False) #Disable warnings
 #Use pin 12 for PWM signal
 pwm_gpio_s = [12,33] 
 frequence = 50
-# now scan the x,y positions
-x_s=[0,90,180]
-y_s=[90,120,150]
 
-center=90
-span=20
-x_s=np.linspace(center-span,center+span,num=5,endpoint=True)
-y_s=np.linspace(center-span,center+span,num=5,endpoint=True)
+if ((cli_x==0) and (cli_y==0)):
+	cli_position=False
+else:
+	cli_position=True
+
+if cli_position:
+	x_s=[cli_x]
+	y_s=[cli_y]
+else:
+	# now scan the x,y positions
+	if span==0:
+		x_s=[0,90,180]
+		y_s=[90,120,150]
+	else:
+		x_s=np.linspace(center-span,center+span,num=steps,endpoint=True)
+		y_s=np.linspace(center-span,center+span,num=steps,endpoint=True)
 
 GPIO.setup(pwm_gpio_s[0], GPIO.OUT)
 GPIO.setup(pwm_gpio_s[1], GPIO.OUT)
