a7a4e179a77e5eeff20f0aa096e6687c0b4b0666
foice <foice.news@gmail.com>
2022-06-12 11:51:52 +0200

    Added reading from HomeAssistant Logs

--- AC.sh
+++ AC.sh
@@ -15,40 +15,51 @@ echo "Will keep T=$temp °C"
 
 while [ 1 -ge 0 ]
 do
- 
-~/rasp_scripts/connect_wlanX.sh -i wlan0 -s $subnet 
-# -q quiet
-# -c nb of pings to perform
-ping -q -c5 $host  > /dev/null
- 
-	if [ $? -eq 0 ]
+	# try to get tempearture from HomeAssistant Logs
+	filetemperature="/home/homeassistant/.homeassistant/temperature_desk.csv"
+	lastlog=`stat -c %Y $filetemperature`
+	now=`date +%s`
+	let age=$now-$lastlog
+	echo "File age is $age seconds"
+	if [ $age -lt 2400 ]  #40 minutes
 	then
-		echo "Host is reachable, sleeping $sl"
-		sleep $sl
-		url="$host"/last_temp.csv
-		echo "URL $url"
-		wget -d --tries=4 --read-timeout=3 -O "last" "$host"/last_temp.csv 
-		#curl -L -C - -o "last" $url  #"$host"/last_temp.csv
-		#lynx -source $url > last  
-		current_temperature=$(cat last | cut -f2 -d"," | cut -f1 -d".")
-		echo "Temperature from $host : $current_temperature"
+		current_temperature=$(tail -1 /home/homeassistant/.homeassistant/temperature_desk.csv | cut -f2 -d",")		
+		echo "Temperature from $filetemperature : $current_temperature"
 	else
-		#trying to read call-up log
-		lastlog=`stat -c %Y /home/pi/temperature.log`
-		now=`date +%s`
-		let age=$now-$lastlog
-		echo "File age is $age seconds"
-		if [ $age -lt 2400 ]  #40 minutes
+		# try to get temperature from WLAN direct connection to the ESP8266 
+		~/rasp_scripts/connect_wlanX.sh -i wlan0 -s $subnet 
+		# -q quiet
+		# -c nb of pings to perform
+		ping -q -c5 $host  > /dev/null
+		 
+		if [ $? -eq 0 ]
 		then
-			current_temperature=`less /home/pi/temperature.log | grep Appending | tail -1 | cut -f2 -d":" | cut -f2 -d","` # | cut -f1 -d"."`	
-			echo "Temperature from callup: $current_temperature ($age seconds ago)"
+			echo "Host is reachable, sleeping $sl"
+			sleep $sl
+			url="$host"/last_temp.csv
+			echo "URL $url"
+			wget -d --tries=4 --read-timeout=3 -O "last" "$host"/last_temp.csv 
+			#curl -L -C - -o "last" $url  #"$host"/last_temp.csv
+			#lynx -source $url > last  
+			current_temperature=$(cat last | cut -f2 -d"," | cut -f1 -d".")
+			echo "Temperature from $host : $current_temperature"
 		else
-			curl wttr.in | head -4 | tail -1 > t		
-			current_temperature=$(more t | sed 's/\x1B\[[0-9;]\+[A-Za-z]//g'  | cut -f3 -d"." | tr -d " " | cut -c1-2)
-			echo "Going from weather: $current_temperature"
+			#trying to read call-up log
+			lastlog=`stat -c %Y /home/pi/temperature.log`
+			now=`date +%s`
+			let age=$now-$lastlog
+			echo "File age is $age seconds"
+			if [ $age -lt 2400 ]  #40 minutes
+			then
+				current_temperature=`less /home/pi/temperature.log | grep Appending | tail -1 | cut -f2 -d":" | cut -f2 -d","` # | cut -f1 -d"."`	
+				echo "Temperature from callup: $current_temperature ($age seconds ago)"
+			else
+				curl wttr.in | head -4 | tail -1 > t		
+				current_temperature=$(more t | sed 's/\x1B\[[0-9;]\+[A-Za-z]//g'  | cut -f3 -d"." | tr -d " " | cut -c1-2)
+				echo "Going from weather: $current_temperature"
+			fi
 		fi
-	fi
-	
+	fi	
 	echo "The current temperature is $current_temperature °C"
 
 	if (( $(bc -l <<<"$current_temperature >= $temp") )); then
