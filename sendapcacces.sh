065a89018319d62e66950dd386e0980cf9ed5a33
foice <foice.news@gmail.com>
2022-10-25 17:25:53 +0200

    hostname into subject

--- sendapcacces.sh
+++ sendapcacces.sh
@@ -1,3 +1,3 @@
 email_recipient=$1
 status=$2
-echo "Subject: UPS status message: $2" | cat - $(apcaccess | tr -d ":" > ~/apc.log) ~/apc.log    | /usr/bin/msmtp --file=/home/pi/.msmtprc -a fisica -- "$email_recipient" 
+echo "Subject: $HOSTNAME UPS status message: $2" | cat - $(apcaccess | tr -d ":" > ~/apc.log) ~/apc.log    | /usr/bin/msmtp --file=/home/pi/.msmtprc -a fisica -- "$email_recipient" 
